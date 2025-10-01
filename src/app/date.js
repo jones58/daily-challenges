3d0f136c75264203b14ecc6a15a2cf03ddc9c87b
jackkershaw <hello@jackkershaw.net>
2025-10-01 14:19:50 +0100

    get current date effectively on client side rather than at build time

--- src/app/date.js
+++ src/app/date.js
@@ -1,7 +1,13 @@
-const currentDate = new Date().toLocaleString("default", {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-});
+"use client";
+
+const getCurrentDate = () => {
+  return new Date().toLocaleString("default", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+const currentDate = getCurrentDate();
 
 export default currentDate;
